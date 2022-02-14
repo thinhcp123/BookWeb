be4b0ffb11752a31027751528fa9aa08cb378b2b
Tran Duc Thinh <Thangthinhthoi09@gmail.com>
2022-02-14 20:58:26 +0700

    Add project files.

---
+++ BookWeb/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookWeb.Models;
+public class Category
+{
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    public string Name { get; set; }
+    public int DisplayOrder { get; set; }
+    public DateTime CreateDateTime { get; set; } = DateTime.Now;
+}
